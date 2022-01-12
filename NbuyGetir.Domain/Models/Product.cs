6240aa84fe4d524e8415c7b84bc52b7e92e09e70
yasindurgun <yasin.durgun52@gmail.com>
2022-01-12 19:02:30 +0300

    Product operations and common layer created

--- NbuyGetir.Domain/Models/Product.cs
+++ NbuyGetir.Domain/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using NbuyGetir.Core.Entities;
+﻿using NbuyGetir.Common.Uri;
+using NbuyGetir.Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,18 +10,38 @@ namespace NbuyGetir.Domain.Models
 {
     public class Product : AuditableEntity
     {
-        public Product(string name, decimal unitPrice, decimal listPrice, int stock, string description, string pictureBase64, string pictureUrl)
+        public Product(string name, decimal unitPrice, decimal listPrice, int stock, string description, string pictureUrl, decimal discountedListPrice)
         {
-            
+            SetName(name);
+            SetPrice(unitPrice,listPrice,discountedListPrice);
+            SetDescription(description);
+            SetStock(stock);
         }
-
+        /// <summary>
+        /// Hem kayıt hemde güncelleme işleminde kullanılacağından dolayı public
+        /// </summary>
+        public void SetPictureUrl(string url)
+        {
+            if (!UrlHelper.IsUrl(url))
+            {
+                throw new Exception("Resim yolu url formatında değildir");
+            }
+            if (string.IsNullOrEmpty(url))
+            {
+                PictureUrl = "default-product.jpeg";
+            }
+            else
+            {
+                PictureUrl = url.Trim();
+            }
+        }
+       
         public string Name { get; set; }
         public decimal UnitPrice { get; set; }
         public decimal ListPrice { get; private set; }
         public decimal DiscountedListPrice { get; private set; }
         public int Stock { get; set; }
         public string Description { get; set; } //10x1, 1lt, 2kg vs
-        public string PictureBase64 { get; private set; }
         public string PictureUrl { get; set; }
         public bool IsDiscounted { get
             {
@@ -28,7 +49,11 @@ namespace NbuyGetir.Domain.Models
             }
         }
 
-        private void SetName(string name)
+        public bool IsStockInCriticalLevel { get {
+                return Stock < 10 ? true : false;
+            } }
+
+        public void SetName(string name)
         {
             if (string.IsNullOrEmpty(name))
             {
@@ -36,19 +61,87 @@ namespace NbuyGetir.Domain.Models
             }
             Name = name.Trim();
         }
+        private void SetStock(int stock)
+        {
+            if (stock < 0)
+            {
+                throw new Exception("Stok değeri 0'dan daha küçük olamaz.");
+            }
+          
+            Stock = stock;
+        }
+        /// <summary>
+        /// Stoklama işlemi, ürünün envantere girmesi işlemi
+        /// </summary>
+        public void StockIn(int quantity)
+        {
+            if (quantity <= 0)
+            {
+                throw new Exception("Stoğa girilecek yeni ürün adeti 0 ve daha düşük olamaz.");
+            }
+            Stock += quantity;
+            //Stoğa ürün girildi eventi fırlatalım.
+        }
 
-        private void SetPrice(decimal unitPrice, decimal listPrice)
+        public void StockOut(int quantity)
+        {
+            if (quantity < 0)
+            {
+                throw new Exception("0 dan küçük değer stokdan düşülemez.");
+            }
+            if (IsStockInCriticalLevel)
+            {
+                //kritik sok seviyesindeki bir ürün sipariş edildi diye bir mesaj atalım.
+            }
+            if (quantity>Stock)
+            {
+                //hata
+                throw new Exception("Stoktan düşülen miktar stoktaki miktardan büyük olamaz.");
+            }
+
+            Stock -= quantity;
+        }
+
+        public void SetDescription(string description)
+        {
+            if (string.IsNullOrEmpty(Description))
+            {
+                throw new Exception("Ürün açıklama alanını doldurunuz.");
+            }
+            if (description.Length > 50)
+            {
+                throw new Exception("Maksimum 50 karakter girebilirsiniz.");
+            }
+            Description = description;
+        }
+        /// <summary>
+        /// Ürüne ait fiyatların değişimini bu method ile yapacağız.
+        /// </summary>
+        /// <param name="unitPrice"></param>
+        /// <param name="listPrice"></param>
+        /// <param name="discountedListPrice"></param>
+        public void SetPrice(decimal unitPrice, decimal listPrice, decimal discountedListPrice)
         {
             if (unitPrice > listPrice)
             {
                 throw new Exception("Birim fiyat liste fiyatından büyük olamaz.");
             }
-            if (unitPrice<=0 || listPrice<=0)
+            if (unitPrice<=0 || listPrice<=0 || discountedListPrice<=0)
             {
-                throw new Exception("Üürn birim fiyatı ya da ürün satış fiyatı negatif ve 0 verilemez.");
+                throw new Exception("Üürn birim fiyatı ya da ürün satış fiyatı ya da indirimli satış fiyatı negatif veya 0 verilemez.");
+            }
+            if (discountedListPrice > listPrice)
+            {
+                throw new Exception("İndirimli satış fiyatı satış fiyatından büyük olamaz.");
+            }
+            if (discountedListPrice<unitPrice)
+            {
+                throw new Exception("İndirimli satış fiyatı birim fiyatından küçük olamaz.");
+
             }
             ListPrice = listPrice;
             UnitPrice = unitPrice;
+            DiscountedListPrice = discountedListPrice;
         }
         /// <summary>
         /// Bu bethod ile bir ürünğn satış fiyatona belirli bir oranda indirim yapılır.
@@ -56,7 +149,7 @@ namespace NbuyGetir.Domain.Models
         /// Bu indirimli fiyatımız olacaktır.
         /// </summary>
         /// <param name="discountAmount"></param>
-        private void DescreasePrice(decimal newPrice)
+        public void DescreasePrice(decimal newPrice)
         {
             if (newPrice > ListPrice)
             {
