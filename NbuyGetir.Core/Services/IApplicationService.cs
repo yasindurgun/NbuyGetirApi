8a579d87673354f5899d502167cc81c4d2a11cbd
yasindurgun <yasin.durgun52@gmail.com>
2022-01-14 15:50:28 +0300

    Change trequestdto to tresponsedto

--- NbuyGetir.Core/Services/IApplicationService.cs
+++ NbuyGetir.Core/Services/IApplicationService.cs
@@ -10,9 +10,9 @@ namespace NbuyGetir.Core.Services
     /// Frontend tarafından gelen bir isteğin işlenip frontend tarafına bir sonucun döndürülmesi için yaptık. Burada apide inputmodel ve viewmodel yerine artık dto terimini kullanacağız. Buradaki servisi application katmanı için yazıyoruz.
     /// </summary>
     /// <typeparam name="TRequestDto">InputModel</typeparam>
-    /// <typeparam name="TResultDto">ViewModel olarak kullanılacak.</typeparam>
-    public interface IApplicationService<TRequestDto,TResultDto>
+    /// <typeparam name="TResponseDto">ViewModel olarak kullanılacak.</typeparam>
+    public interface IApplicationService<TRequestDto,TResponseDto>
     {
-        Task<TResultDto> HandleAsync(TResultDto dto);
+        Task<TResponseDto> HandleAsync(TResponseDto dto);
     }
 }
